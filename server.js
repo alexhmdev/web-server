2526f36c1532ff62ea7de4c154db1d77514240ed
Alex <alex13pks@gmail.com>
2019-10-18 10:33:57 -0500

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const hbs = require('hbs');
 
-
+//heroku
 app.use(express.static(__dirname + '/public'));
 hbs.registerPartials(__dirname + '/views/partials');
  
