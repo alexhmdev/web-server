b061ef462a1dc76dc3dd19df1ca2db219133f568
Alex <alex13pks@gmail.com>
2019-10-18 09:51:17 -0500

    make it better

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials');
+ 
+app.set('view engine', 'hbs');
+
+app.get('/', (req,res) =>{
+    res.render('home',{
+        nombre: 'Alex'
+    });
+});
+app.get('/about', (req,res) =>{
+    res.render('about');
+});
+// app.get('/', function (req, res) {
+// //   res.send('<h1>Hello world</h1>')
+// let salida = {
+//     nombre: "AlexHM",
+//     edad: 18,
+//     url: req.url
+// };
+
+// res.send(salida);
+// });
+
+// app.get('/hola', (req, res) =>{
+//     res.send('Estas en hola');
+// });
+ 
+app.listen(3000);
