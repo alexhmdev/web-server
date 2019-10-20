d4152737d065856c4c1b93040faccff82a5ce436
Alex <alex13pks@gmail.com>
2019-10-20 02:03:51 -0500

    let PORT is now a constant

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
-let PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 //heroku redy
 app.use(express.static(__dirname + '/public'));
